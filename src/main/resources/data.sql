ddb16798e5a0a70624e05940ab64717e991390bd
florin.pop <florin_pop85@yahoo.com>
2022-03-01 14:48:34 +0200

    Eighth_Commit: added ReservationControllerTest.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,8 +4,28 @@ insert into guest(id, name) values(null, 'Rafael Nadal');
 insert into tennis_court(id, name) values(null, 'Roland Garros - Court Philippe-Chatrier');
 
 insert
-    into
-        schedule
-        (id, start_date_time, end_date_time, tennis_court_id)
-    values
-        (null, '2020-12-20T20:00:00.0', '2020-02-20T21:00:00.0', 1);
+    into schedule(id, start_date_time, end_date_time, tennis_court_id)
+    values(null, '2020-12-20T20:00:00.0', '2020-02-20T21:00:00.0', 1);
+insert
+    into schedule(id, start_date_time, end_date_time, tennis_court_id)
+    values(null, '2030-12-01T10:00:00.0', '2030-12-01T11:00:00.0', 1);
+insert
+    into schedule(id, start_date_time, end_date_time, tennis_court_id)
+    values(null, '2030-12-01T12:00:00.0', '2030-12-01T13:00:00.0', 1);
+insert
+    into schedule(id, start_date_time, end_date_time, tennis_court_id)
+    values(null, '2030-12-01T18:00:00.0', '2030-12-01T19:00:00.0', 1);
+insert
+    into schedule(id, start_date_time, end_date_time, tennis_court_id)
+    values(null, '2030-12-01T21:00:00.0', '2030-12-01T22:00:00.0', 1);
+
+
+insert
+    into reservation(id, refund_value, reservation_status, value, guest_id, schedule_id)
+    values(null, 0, 0, 10, 1, 1);
+insert
+    into reservation(id, refund_value, reservation_status, value, guest_id, schedule_id)
+    values(null, 10, 0, 10, 1, 2);
+insert
+    into reservation(id, refund_value, reservation_status, value, guest_id, schedule_id)
+    values(null, 10, 0, 10, 1, 3);
---
+++ src/test/java/com/tenniscourts/reservations/ReservationControllerTest.java
@@ -0,0 +1,97 @@
+package com.tenniscourts.reservations;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tenniscourts.guests.GuestDTO;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ReservationControllerTest {
+
+    @Autowired
+    public ObjectMapper objectMapper;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void bookReservation() throws Exception {
+        mockMvc.perform(post("/reservations").contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(CreateReservationRequestDTO.builder().guestId(1L).scheduleId(4L).build())))
+                .andExpect(status().isCreated())
+                .andReturn();
+    }
+
+    @Test
+    public void cancelReservation() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(put("/reservations/cancel/2"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        ReservationDTO reservationDTO = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), ReservationDTO.class);
+        assertEquals(2, reservationDTO.getId());
+        assertEquals(ReservationStatus.CANCELLED.name(), reservationDTO.getReservationStatus());
+    }
+
+    @Test
+    public void findAllPastReservations() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(get("/reservations/past-reservations"))
+                .andExpect(status().isOk()).andReturn();
+
+        List<ReservationDTO> reservationDTOs = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), new TypeReference<>() {});
+        assertEquals(1, reservationDTOs.size());
+        assertEquals(1, reservationDTOs.get(0).getId());
+    }
+
+    @Test
+    public void findReservation() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(get("/reservations/1"))
+                .andExpect(status().isOk()).andReturn();
+
+        ReservationDTO reservationDTO = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), ReservationDTO.class);
+        assertEquals(1, reservationDTO.getId());
+    }
+
+    @Test
+    public void findReservation_notFound() throws Exception {
+        mockMvc.perform(get("/reservations/100"))
+                .andExpect(status().isNotFound())
+                .andExpect(result -> assertEquals("Reservation not found.", result.getResolvedException().getMessage()));
+    }
+
+    @Test
+    public void rescheduleReservation() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(put("/reservations/reschedule?" + "reservationId=4" + "&scheduleId=5"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        ReservationDTO reservationDTO = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), ReservationDTO.class);
+        assertEquals(5, reservationDTO.getId());
+        assertEquals(5, reservationDTO.getSchedule().getId());
+        assertEquals(ReservationStatus.READY_TO_PLAY.name(), reservationDTO.getReservationStatus());
+        ReservationDTO previousReservation = reservationDTO.getPreviousReservation();
+        assertEquals(4, previousReservation.getId());
+        assertEquals(4, previousReservation.getSchedule().getId());
+        assertEquals(ReservationStatus.RESCHEDULED.name(), previousReservation.getReservationStatus());
+    }
+}
